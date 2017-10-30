44f4c056fc9f262416783b2ab21f2032eef980f1
John-Mike Marquardt <jmmarquardt@gmail.com>
2017-10-30 00:13:51 -0400

    CHORE Lesson2 md files

--- lesson-2-async-w-jQuery/app.js
+++ lesson-2-async-w-jQuery/app.js
@@ -1,14 +1,68 @@
 /* eslint-env jquery */
-
 (function () {
-    const form = document.querySelector('#search-form');
-    const searchField = document.querySelector('#search-keyword');
-    let searchedForText;
-    const responseContainer = document.querySelector('#response-container');
+  const form = document.querySelector('#search-form');
+  const searchField = document.querySelector('#search-keyword');
+  const responseContainer = document.querySelector('#response-container');
+  const apiID = `Client-ID`;
+  const nytKey = '';
+  let searchedForText;
 
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        responseContainer.innerHTML = '';
-        searchedForText = searchField.value;
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    responseContainer.innerHTML = '';
+    searchedForText = searchField.value;
+
+    $.ajax({
+      url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+      headers: {
+          Authorization: apiID
+      }
+    }).done(addImage)
+    .fail(function (err){
+      requestError(err, 'image');
     });
+
+    $.ajax({ 
+        url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=${nytKey}`
+      })
+      .done(addArticles)
+      .fail(function (err) {
+        requestError(err, 'article')
+      });
+  });
+
+
+  function addImage (images) {
+    let htmlContent = '';
+
+    if (images && images.results && images.results[0]) {
+        const firstImage = images.results[0];
+        console.log(firstImage)
+
+        htmlContent = `<figure>
+        <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+        <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+        </figure>`;
+    } else {
+        htmlContent = '<div class="error-no-image">No images available</div>';
+    }
+
+    responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+  }
+
+  function addArticles (articles) {
+    let htmlContent = '';
+
+    if (articles.response && articles.response.docs && articles.response.docs.length > 1) {
+      htmlContent = `<ul>${articles.response.docs.map(article => `<li class="article">
+        <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+        <p>${article.snippet}</p>
+      </li>`).join('')}</ul>`;
+    } else {
+      htmlContent = '<div class = "error-no-articles">No articles available.</div>';
+    }
+
+    responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+  };
 })();
