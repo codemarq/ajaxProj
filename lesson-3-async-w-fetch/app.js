bb0359854fe958aad058214b3dc3df088c072f6e
John-Mike Marquardt <jmmarquardt@gmail.com>
2017-10-31 12:09:44 -0400

    FEAT: fetch(), and addImage both working
    - [x] The fetch function is flushed out to callback `addImage`
    - [x] moved fetch into form event listener
    - [x] removed api key from commit
    - [x] addImage flushed out with error catch, displaying content to page.
    - [x] requestError()

--- lesson-3-async-w-fetch/app.js
+++ lesson-3-async-w-fetch/app.js
@@ -1,12 +1,44 @@
 (function () {
     const form = document.querySelector('#search-form');
     const searchField = document.querySelector('#search-keyword');
-    let searchedForText;
     const responseContainer = document.querySelector('#response-container');
+    const appid = '72edb3e6540e522d1fff7cf6cac4b522d657d63be6c99a5de717a11f5a619442';
+    const requestHeaders = new Headers();
+    let searchedForText;
 
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+        requestHeaders.append('Authorization', `Client-ID ${appid}`);
+
+        fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`, {
+             headers: requestHeaders
+        }).then(response => response.json())
+        .then(addImage)
+        .catch(e => requestError(e, 'image'));
     });
+
+    
+
+    function addImage(data) {
+        let htmlContent = '';
+        const firstImage = data.results[0];
+    
+        if (firstImage) {
+            htmlContent = `<figure>
+                <img src="${firstImage.urls.small}" alt="${searchedForText}">
+                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+            </figure>`;
+        } else {
+            htmlContent = 'Unfortunately, no image was returned for your search.'
+        }
+    
+        responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    }
+
+    function requestError(e, part) {
+        console.log(e);
+        responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning">Oh no! There was an error making a request for the ${part}.</p>`);
+    }
 })();
